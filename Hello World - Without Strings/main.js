cd58005008425052dfe6fda22e7aaf10050f25a8
Akeem Chambers <chambersakeem@gmail.com>
2023-04-24 20:09:33 -0400

    update

--- Hello World - Without Strings/main.js
+++ Hello World - Without Strings/main.js
@@ -10,6 +10,5 @@
 // Solution:
 
 const helloWorld = () => {
-//  create function that will return the String "Hello, World"
-return String.fromCharCode(72,101,108,108,111,44,32,87,111,114,108,100,33)
-};
+    return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33)
+   };
