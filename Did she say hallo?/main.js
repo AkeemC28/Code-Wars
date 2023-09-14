897544b5cd1c8d5c98da802ece68b50fe079ae05
Akeem Chambers <chambersakeem@gmail.com>
2023-09-14 01:06:43 -0400

    new codewar

---
+++ Did she say hallo?/main.js
@@ -0,0 +1,38 @@
+// ou received a whatsup message from an unknown number. Could it be from that girl/boy with a foreign accent you met yesterday evening?
+
+// Write a simple function to check if the string contains the word hallo in different languages.
+
+// These are the languages of the possible people you met the night before:
+
+// hello - english
+// ciao - italian
+// salut - french
+// hallo - german
+// hola - spanish
+// ahoj - czech republic
+// czesc - polish
+// Notes
+
+// you can assume the input is a string.
+// to keep this a beginner exercise you don't need to check if the greeting is a subset of word (Hallowen can pass the test)
+// function should be case insensitive to pass the tests
+
+// Solution:
+
+function validateHello(greetings) {
+  let english = 'hello'
+  let italian = 'ciao' 
+  let french = 'salut'
+  let german = 'hallo'
+  let spanish = 'hola'
+  let czechRepublic = 'ahoj'
+  let polish = 'czesc'
+  let languages = [english, italian, french, german, spanish, czechRepublic, polish]
+  
+  for(let i = 0; i < languages.length; i++){
+    if(greetings.toLowerCase().includes(languages[i])){
+      return true
+    }
+  }
+  return false
+}
