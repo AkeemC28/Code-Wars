f28ec66c2f446b0f52c3e95c4f3541b22e512296
Akeem Chambers <chambersakeem@gmail.com>
2023-09-18 21:09:22 -0400

    new codewar

---
+++ Shortest Word/main.js
@@ -0,0 +1,20 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+// Solution:
+
+function findShort(s){
+  let words = s.split(" ")
+  let shortest = words[0].length 
+  
+for (let i = 1; i < words.length; i++) {
+    const currentWord = words[i].length;
+    
+    if (currentWord < shortest) {
+      shortest = currentWord;
+    }
+  }
+
+  return shortest;
+}
